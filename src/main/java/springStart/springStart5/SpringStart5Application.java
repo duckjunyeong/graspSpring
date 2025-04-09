2eab368df8fbe167aebc85520cb822c89d94918d
junyeong <whwnsdud4592@naver.com>
2025-04-09 11:08:35 +0900

    docs : dispatcher servlet 과 handlerMapping 간단하게 구현해보기

---
+++ src/main/java/springStart/springStart5/SpringStart5Application.java
@@ -0,0 +1,13 @@
+package springStart.springStart5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springStart.springStart5.assembler.Assembler;
+
+public class SpringStart5Application {
+
+	public static void main(String[] args) {
+		Assembler assembler = new Assembler();
+	}
+
+}
